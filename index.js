a435156e6587e93e50582fbd36ab7838a71ae39e
OtienohOwinoh <coowinoh@gmail.com>
2022-08-17 07:34:35 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+
+
+// const welcomeMessage = 'Welcome to Flatbook, '
+
+ 
+// const welcomeMessage = "Welcome to Flatbook, currentUser";
+// const welcomeMessage = "Welcome to Flatbook, Grace Hopper!";
+
+const welcomeMessage = "Welcome to Flatbook, {Grace Hopper}!"
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+// const shortGreeting = `Welcome, {currentUser..slice (0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
